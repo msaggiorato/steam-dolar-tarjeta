3cfe74b9d13c93cc6650a6de493593a1ab854521
Matias Saggiorato <matiassaggiorato@gmail.com>
2014-12-21 03:01:27 -0300

    Version Inicial

---
+++ dolar-tarjeta.js
@@ -0,0 +1,241 @@
+
+var xhr = new XMLHttpRequest();
+xhr.onload = function(data) {
+	var dolar = $.parseJSON(substr(xhr.responseText, 19, -2));
+	var dolarTarjeta = parseFloat((parseFloat(dolar["CasaCambioVentaValue"].replace(",",".")) * 1.35).toFixed(3));
+	var timeout;
+	var convertToArsClean = function(price) {
+		var oldPrice = parseFloat(price.replace(" USD", "").replace("$", ""));
+		var newPrice = parseFloat((oldPrice * dolarTarjeta).toFixed(2));
+		return newPrice;
+	}
+	var convertToArs = function(price, symbol) {
+		if(typeof symbol == "undefined")
+			symbol = ""
+		else
+			symbol = " "+symbol;
+
+		var newPrice = convertToArsClean(price);
+		if(isNaN(newPrice))
+			return price;
+
+		return "$"+newPrice.toFixed(2)+symbol;
+	}
+	var check = function(){
+		$(".discount_final_price").not(".tarjetized").each(function(){
+			$(this).html(convertToArs($(this).html(), "ARS")).addClass('tarjetized');
+		});
+		$(".discount_original_price").not(".tarjetized").each(function(){
+			$(this).html(convertToArs($(this).html())).addClass('tarjetized');
+		});
+
+		$(".game_purchase_action_bg .game_purchase_price,"+
+			" #search_suggestion_contents .match_price,"+
+			" #header_wallet_balance,"+
+			" .transactionRowPrice,"+
+			" .cart_item_price .price,"+
+			" #cart_price_total").not(".tarjetized").each(function(){
+			$(this).html(convertToArs($(this).html(), "ARS")).addClass('tarjetized');
+		})
+
+		$(".col.search_price").not(".tarjetized").each(function(){
+			var wholeText = trim($(this).text());
+			var fullPrice = trim($(this).find("span").text());
+			var finalPrice = wholeText.replace(fullPrice, "");
+			var newHTML = "";
+			if(fullPrice != ""){
+				newHTML += '<span style="color: #888888;"><strike>'+"$"+convertToArsClean(fullPrice).toFixed(2)+'</strike></span><br>';
+			}
+			var newPrice = convertToArsClean(finalPrice);
+			newHTML += "$"+newPrice.toFixed(2)+" ARS";
+			if(isNaN(newPrice)){
+				$(this).addClass('tarjetized');
+				return;
+			}
+			$(this).html(newHTML)
+			$(this).addClass('tarjetized');
+		});
+		clearTimeout(timeout);
+		timeout = setTimeout(check, 500);
+	}
+	check();
+
+}
+xhr.open("GET", "http://contenidos.lanacion.com.ar/json/dolar?callback=jsonpCallback", true);
+xhr.send();
+
+
+
+
+
+
+
+
+
+function substr(str, start, len) {
+  //  discuss at: http://phpjs.org/functions/substr/
+  //     version: 909.322
+  // original by: Martijn Wieringa
+  // bugfixed by: T.Wild
+  // improved by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //  revised by: Theriault
+  //        note: Handles rare Unicode characters if 'unicode.semantics' ini (PHP6) is set to 'on'
+  //   example 1: substr('abcdef', 0, -1);
+  //   returns 1: 'abcde'
+  //   example 2: substr(2, 0, -6);
+  //   returns 2: false
+  //   example 3: ini_set('unicode.semantics',  'on');
+  //   example 3: substr('a\uD801\uDC00', 0, -1);
+  //   returns 3: 'a'
+  //   example 4: ini_set('unicode.semantics',  'on');
+  //   example 4: substr('a\uD801\uDC00', 0, 2);
+  //   returns 4: 'a\uD801\uDC00'
+  //   example 5: ini_set('unicode.semantics',  'on');
+  //   example 5: substr('a\uD801\uDC00', -1, 1);
+  //   returns 5: '\uD801\uDC00'
+  //   example 6: ini_set('unicode.semantics',  'on');
+  //   example 6: substr('a\uD801\uDC00z\uD801\uDC00', -3, 2);
+  //   returns 6: '\uD801\uDC00z'
+  //   example 7: ini_set('unicode.semantics',  'on');
+  //   example 7: substr('a\uD801\uDC00z\uD801\uDC00', -3, -1)
+  //   returns 7: '\uD801\uDC00z'
+
+  var i = 0,
+    allBMP = true,
+    es = 0,
+    el = 0,
+    se = 0,
+    ret = '';
+  str += '';
+  var end = str.length;
+
+  // BEGIN REDUNDANT
+  this.php_js = this.php_js || {};
+  this.php_js.ini = this.php_js.ini || {};
+  // END REDUNDANT
+  switch ((this.php_js.ini['unicode.semantics'] && this.php_js.ini['unicode.semantics'].local_value.toLowerCase())) {
+    case 'on':
+      // Full-blown Unicode including non-Basic-Multilingual-Plane characters
+      // strlen()
+      for (i = 0; i < str.length; i++) {
+        if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+          allBMP = false;
+          break;
+        }
+      }
+
+      if (!allBMP) {
+        if (start < 0) {
+          for (i = end - 1, es = (start += end); i >= es; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              start--;
+              es--;
+            }
+          }
+        } else {
+          var surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+          while ((surrogatePairs.exec(str)) != null) {
+            var li = surrogatePairs.lastIndex;
+            if (li - 2 < start) {
+              start++;
+            } else {
+              break;
+            }
+          }
+        }
+
+        if (start >= end || start < 0) {
+          return false;
+        }
+        if (len < 0) {
+          for (i = end - 1, el = (end += len); i >= el; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              end--;
+              el--;
+            }
+          }
+          if (start > end) {
+            return false;
+          }
+          return str.slice(start, end);
+        } else {
+          se = start + len;
+          for (i = start; i < se; i++) {
+            ret += str.charAt(i);
+            if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+              se++; // Go one further, since one of the "characters" is part of a surrogate pair
+            }
+          }
+          return ret;
+        }
+        break;
+      }
+      // Fall-through
+    case 'off':
+      // assumes there are no non-BMP characters;
+      //    if there may be such characters, then it is best to turn it on (critical in true XHTML/XML)
+    default:
+      if (start < 0) {
+        start += end;
+      }
+      end = typeof len === 'undefined' ? end : (len < 0 ? len + end : len + start);
+      // PHP returns false if start does not fall within the string.
+      // PHP returns false if the calculated end comes before the calculated start.
+      // PHP returns an empty string if start and end are the same.
+      // Otherwise, PHP returns the portion of the string from start to end.
+      return start >= str.length || start < 0 || start > end ? !1 : str.slice(start, end);
+  }
+  return undefined; // Please Netbeans
+}
+
+function trim(str, charlist) {
+  //  discuss at: http://phpjs.org/functions/trim/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: mdsjack (http://www.mdsjack.bo.it)
+  // improved by: Alexander Ermolaev (http://snippets.dzone.com/user/AlexanderErmolaev)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Steven Levithan (http://blog.stevenlevithan.com)
+  // improved by: Jack
+  //    input by: Erkekjetter
+  //    input by: DxGx
+  // bugfixed by: Onno Marsman
+  //   example 1: trim('    Kevin van Zonneveld    ');
+  //   returns 1: 'Kevin van Zonneveld'
+  //   example 2: trim('Hello World', 'Hdle');
+  //   returns 2: 'o Wor'
+  //   example 3: trim(16, 1);
+  //   returns 3: 6
+
+  var whitespace, l = 0,
+    i = 0;
+  str += '';
+
+  if (!charlist) {
+    // default list
+    whitespace =
+      ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+  } else {
+    // preg_quote custom list
+    charlist += '';
+    whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1');
+  }
+
+  l = str.length;
+  for (i = 0; i < l; i++) {
+    if (whitespace.indexOf(str.charAt(i)) === -1) {
+      str = str.substring(i);
+      break;
+    }
+  }
+
+  l = str.length;
+  for (i = l - 1; i >= 0; i--) {
+    if (whitespace.indexOf(str.charAt(i)) === -1) {
+      str = str.substring(0, i + 1);
+      break;
+    }
+  }
+
+  return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+}
